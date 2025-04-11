6941bdd441b13ca1a6a995c8d5e2bfff497f7759
Sebs <seva32@yahoo.es>
2025-04-11 13:12:35 -0400

    ignores

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,3 +1,9 @@
-import baseConfig from '../eslint.config.mjs';
+import baseConfig from '../eslint.config.mjs'
 
-export default [...baseConfig];
+export default [
+  ...baseConfig,
+  {
+    files: ['src/**/*.{ts,tsx,js,jsx}'],
+    rules: {},
+  },
+]
