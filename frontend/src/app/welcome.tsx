1f69f7d5d894ec30d30b65c2f380505d1c75e99c
Sebs <seva32@yahoo.es>
2025-04-11 11:49:06 -0400

    resolve ci scripts

---
+++ frontend/src/app/welcome.tsx
@@ -0,0 +1,12 @@
+export function Welcome({ title }: { title: string }) {
+  return (
+    <div>
+      <h1>Welcome {title}!</h1>
+      <div />
+      <p>
+        To get started, edit <code>src/app/welcome.tsx</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+export default Welcome;
