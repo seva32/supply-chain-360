82ce38baaf6224118ef26890e7b4a755fe9c7352
Sebs <seva32@yahoo.es>
2025-04-11 11:49:06 -0400

    resolve ci scripts

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,6 +1,6 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import NxWelcome from './welcome';
 
 export function App() {
   return (
